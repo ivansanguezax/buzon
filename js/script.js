b8d73f18f1d26fb4b9b86dc1ebeee45a804a9799
Ivan Sangueza <iwancho23@gmail.com>
2020-11-29 17:29:53 -0400

    Cambios en links

--- js/script.js
+++ js/script.js
@@ -15,36 +15,36 @@ function buttonChange( that ){
 //Funcion que recibe las sugerencias y las lleva a servidor local con npm
 
 
-const $form = document.querySelector('#contact')
+// const $form = document.querySelector('#contact')
 
-const $image = document.querySelector('#image')
-const $file = document.querySelector('#file')
-function renderImage(formData) {
-  const file = formData.get('image')
-  const image = URL.createObjectURL(file)
-  $image.setAttribute('src', image)
-}
+// const $image = document.querySelector('#image')
+// const $file = document.querySelector('#file')
+// function renderImage(formData) {
+//   const file = formData.get('image')
+//   const image = URL.createObjectURL(file)
+//   $image.setAttribute('src', image)
+// }
 
-const $username = document.querySelector('#Nombre')
-function renderUsername(formData) {
-  const username = formData.get('Nombre')
-  $username.textContent = username
-}
+// const $username = document.querySelector('#Nombre')
+// function renderUsername(formData) {
+//   const username = formData.get('Nombre')
+//   $username.textContent = username
+// }
 
-$file.addEventListener('change', (event) => {
-  const formData = new FormData($contact)
-  renderImage(formData)
-})
+// $file.addEventListener('change', (event) => {
+//   const formData = new FormData($contact)
+//   renderImage(formData)
+// })
 
-$contact.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const formData = new FormData(event.currentTarget)
-  renderUsername(formData)
-  fetch('/', {
-    method: 'POST',
-    body: formData,
-  })
-})
+// $contact.addEventListener('submit', (event) => {
+//   event.preventDefault()
+//   const formData = new FormData(event.currentTarget)
+//   renderUsername(formData)
+//   fetch('/', {
+//     method: 'POST',
+//     body: formData,
+//   })
+// })
 
 
 // admin
